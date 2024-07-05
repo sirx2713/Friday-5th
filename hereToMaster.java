ef6d604698872d0f0b15b8db491b7524f0d9dd51
iSAD <bradx2713@gmail.com>
2024-07-05 07:02:31 +0200

    Add files via upload

---
+++ hereToMaster.java
@@ -0,0 +1,18 @@
+package masteringJava;
+
+public class hereToMaster {
+
+	public static void main(String[] args) {
+		var dayCode = "Friday ";
+		System.out.print(dayCode);
+		
+		var dateCoded = new hereToMaster();
+		dateCoded.codeMore();
+
+	}
+	
+	public void codeMore() {
+		System.out.println(5 + " July" + " 2024");
+	}
+
+}
